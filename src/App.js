8c03c9fac2c209381ddd530ee0a5b2627cc54ae2
Muhammad Ikram <lalatputih22@gmail.com>
2022-10-09 04:43:14 +0800

    secoond

--- src/App.js
+++ src/App.js
@@ -5,21 +5,38 @@ import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import Login from "./ModalLogin";
 import Room from "./page/Room";
 import ModalLogin from "./ModalLogin";
-import { Button } from "flowbite-react";
+import { Button, Card, Spinner } from "flowbite-react";
+import { deck } from "./Deck";
 
 const socket = io.connect("http://localhost:3001");
 
 function App() {
   let id = "";
+
+  console.log("deck", deck);
   socket.on("connect", () => {
     console.log("id saya", socket.id);
     localStorage.setItem("id", socket.id);
   });
 
   const [isHost, setIsHost] = useState(false);
+  const [gameDeck, setGameDeck] = useState(deck);
   const [user, setUser] = useState("");
   const [listUser, setListUser] = useState([]);
+  const [room, setRoom] = useState(null);
+  const [gameCanvas, setGameCanvas] = useState(false);
+
+  const [startGame, setStartGame] = useState(false);
+  const [isPlayer, setIsPlayer] = useState(false);
+  const [showGame, setShowGame] = useState(false);
+
+  console.log("isHost", isHost);
+
   const joinRoom = (room) => {
+    setRoom(room);
+    setGameCanvas(true);
+    setIsPlayer(true);
+
     socket.emit("join_room", room);
   };
 
@@ -40,57 +57,120 @@ function App() {
   };
 
   const getIsHost = () => {
-    if (listUser == 2) {
+    if (listUser.length > 1) {
       console.log("cukuppppppp");
-        if (listUser[0] == localStorage.getItem("id")) {
-          setIsHost(true);
-          console.log("pemain cukup");
-        }
+      if (listUser[0] == localStorage.getItem("id")) {
+        setIsHost(true);
+        console.log("pemain cukup");
+      }
     }
   };
 
+  const gameStart = () => {
+    console.log("host start the game");
+    socket.emit("start_game", { room: room, started: true });
+    setGameCanvas(false);
+    setShowGame(true);
+  };
+
   useEffect(() => {
-    // roomHosted()
-    socket.on("hosted", (data) => {
-      console.log("hosted", data);
-      setIsHost(false);
+    socket.on("test", (data) => {
+      console.log("testdata", data);
     });
 
     newUser();
     getlistUser();
+
+    socket.on("start", (data) => {
+      console.log("started game");
+      setGameCanvas(false);
+      setShowGame(true);
+    });
   }, []);
 
   useEffect(() => {
-      // After 3 seconds set the show value to false
-      const timeId = setTimeout(() => {
-        // After 3 seconds set the show value to false
-        getIsHost();
-      }, 3000);
-  
-      return () => {
-        clearTimeout(timeId);
-      };
-    
+    getIsHost();
   }, [listUser]);
+
+  useEffect(() => {}, [startGame]);
 
   return (
     <>
       <ModalLogin joinRoom={joinRoom} />
-      <div>
-        {listUser.map((data) => (
-          <>
-            {data}
-            <br />
-          </>
-        ))}
-      </div>
-
-      {isHost ? (
+      {gameCanvas ? (
         <>
-          <Button>Start now!</Button>
+          <div className="w-screen h-screen flex flex-col justify-center items-center">
+            <div className="w-1/2 justify-center items-center">
+              <Card>
+                <div className="flex flex-col justify-center items-center">
+                  {isHost ? (
+                    <>
+                      <h3 className="text-lg font-bold">List User</h3>
+                      {listUser.map((data) => (
+                        <>
+                          {data}
+                          <br />
+                        </>
+                      ))}
+                      <br></br>
+                      <button
+                        onClick={() => gameStart()}
+                        className="bg-red-500 py-3 px-5 font-bold rounded-md text-white hover:opacity-90"
+                      >
+                        START NOW !
+                      </button>
+                    </>
+                  ) : (
+                    <>
+                      {isPlayer ? (
+                        <>
+                          <Spinner color={"failure"}></Spinner>
+                          <h3 className="font-semibold mt-2">
+                            Waiting the host to start game
+                          </h3>{" "}
+                        </>
+                      ) : (
+                        ""
+                      )}
+                    </>
+                  )}
+                </div>
+              </Card>
+            </div>
+          </div>
         </>
       ) : (
-        ""
+        <>
+          {showGame ? (
+            <>
+              <div className="w-full h-full my-10 flex flex-col justify-center items-center">
+                <div className="w-3/4 justify-center items-center">
+                  <Card>
+                    <div className="flex flex-col justify-center items-center">
+                      <div className="p-2 m-2 shadow-md rounded-lg w-32 flex justify-center mb-20">
+                        <img src={gameDeck[0].img} width="100"></img>
+                      </div>
+                      <div className="flex flex-row flex-wrap justify-center">
+                        <br></br>
+                        {gameDeck.map((data) => {
+                          return (
+                            <>
+                              <div className="p-2 m-2 shadow-md rounded-lg hover:shadow-red-400 hover:cursor-pointer hover:-mt-2 duration-100">
+                                <img src={data.img} width="50"></img>
+                              </div>
+                            </>
+                          );
+                        })}
+                      </div>
+                    </div>
+                  </Card>
+                </div>
+              </div>
+            </>
+          ) : (
+            ""
+          )}
+        </>
       )}
     </>
   );
---
+++ src/Deck.js
@@ -0,0 +1,427 @@
+export const deck = [
+    {
+        color: "Y",
+        value: "0",
+        img: "./images/y0.png"
+    }, 
+    {
+        color: "Y",
+        value: "1",
+        img: "./images/y1.png"
+    },
+    {
+        color: "Y",
+        value: "2",
+        img: "./images/y2.png"
+    },
+    {
+        color: "Y",
+        value: "3",
+        img: "./images/y3.png"
+    },
+    {
+        color: "Y",
+        number: "4",
+        img: "./images/y4.png"
+    },
+    {
+        color: "Y",
+        number: "5",
+        img: "./images/y5.png"
+    },
+    {
+        color: "Y",
+        number: "6",
+        img: "./images/y6.png"
+    },
+    {
+        color: "Y",
+        number: "7",
+        img: "./images/y7.png"
+    },
+    {
+        color: "Y",
+        number: "8",
+        img: "./images/y8.png"
+    },
+    {
+        color: "Y",
+        number: "9",
+        img: "./images/y9.png"
+    },
+    {
+        color: "Y",
+        number: "p2",
+        img: "./images/yp2.png"
+    },
+    {
+        color: "Y",
+        number: "R",
+        img: "./images/yR.png"
+    },
+    {
+        color: "Y",
+        code: "S",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+    {
+        color: "blue",
+        number: "0",
+        img: "./images/yS.png"
+    },
+];
