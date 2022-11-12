2423d8cbed835461a3e4f34b548a3352e014c286
Muhammad Ikram <lalatputih22@gmail.com>
2022-11-13 01:50:32 +0800

    conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,13 @@ module.exports = {
     screens: {
       'xs': '410px',
       // => @media (min-width: 640px) { ... }
-      'sm': '640px',
+      'sm': '1080px',
       // => @media (min-width: 640px) { ... }
 
-      'md': '768px',
+      // 'md': '768px',
       // => @media (min-width: 768px) { ... }
 
-      'lg': '1024px',
+      // 'lg': '1024px',
       // => @media (min-width: 1024px) { ... }
 
       'xl': '1280px',
