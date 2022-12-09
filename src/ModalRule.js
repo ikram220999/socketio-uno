aaca809fbd5e0671387b9eede003f41d302f9d7a
ikram dev <lalatputih22@gmail.com>
2022-12-09 23:30:08 +0800

    rule modify

--- src/ModalRule.js
+++ src/ModalRule.js
@@ -38,38 +38,41 @@ const ModalRule = ({ closeRule }) => {
             <p>Game start after player 1 draw a card</p>
             <hr className="my-4 h-px bg-gray-200 border-0 dark:bg-gray-700" />
 
-            <h2 className="font-semibold text-xl text-center ">
-            Start game
-          </h2>
+            <h2 className="font-semibold text-xl text-center ">Start game</h2>
 
-          
-          <div className="mt-2 flex flex-col gap-4">
-            <p>
-              Enter the player name and room number then press enter
-            </p>
-            <p>Share the room number with other member </p>
-            <p>The game host will see a list of player id that already entered the room</p>
-            <p>Wait till all player already in the list</p>
-            <p>Start the game</p>
-            <p>All palyer will be given 7 card each</p>
-            <p>Host can start the game</p>
-            <p>have fun !</p>
-           
+            <div className="mt-2 flex flex-col gap-4">
+              <p>Enter the player name and room number then press enter</p>
+              <p>Share the room number with other member </p>
+              <p>
+                The game host will see a list of player id that already entered
+                the room
+              </p>
+              <p>Wait till all player already in the list</p>
+              <p>Start the game</p>
+              <p>All palyer will be given 7 card each</p>
+              <p>Host can start the game</p>
+              <p>have fun !</p>
+            </div>
 
+            <div className="bg-red-100 border-2 border-gray-300 ">
+              <b>Important !</b>
+              <p>
+                After complete a game. Please refresh and click reset to avoid
+                any caching problem
+              </p>
+            </div>
 
+            <button
+              className="border-2-rounded bg-blue-400 py-2 px-4 rounded-md text-white font-bold mt-4 w-full mt-10 hover:opacity-90 "
+              onClick={() => closeRulee()}
+            >
+              Back
+            </button>
           </div>
-
-          <button
-            className="border-2-rounded bg-blue-400 py-2 px-4 rounded-md text-white font-bold mt-4 w-full mt-10 hover:opacity-90 "
-            onClick={() => closeRulee()}
-          >
-            Back
-          </button>
         </div>
       </div>
-      </div>
     </>
-  )
+  );
 };
 
 export default ModalRule;
