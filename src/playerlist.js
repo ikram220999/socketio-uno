f6931bf55e00f5d2ff1c2d66b93e7977e1fc2f09
ikram dev <lalatputih22@gmail.com>
2023-02-06 23:12:37 +0800

    add help section

--- src/playerlist.js
+++ src/playerlist.js
@@ -1,14 +1,62 @@
-import React from "react";
+import { Modal } from "flowbite-react";
+import React, { useState } from "react";
 
 const PlayerList = (props) => {
+  const [show, setShow] = useState(false);
   const turn = props.turn;
   const listPlayer = JSON.parse(localStorage.getItem("listUser"));
   const direction = localStorage.getItem("turnDirection");
   const id = localStorage.getItem("id");
 
+  const showModal = () => {
+    setShow(true)
+  }
   return (
     <>
+    <Modal show={show}>
+      <Modal.Body>
+          <div>
+            <p className="text-lg font-semibold">Help ?</p>
+              <hr className="my-2"/>
+            <div>
+              <p className="text-sm mb-2" >Card Type</p>
+              <table className="w-full" border={1}>
+                <tr>
+                  <th className="w-1/5 border text-sm">Card</th>
+                  <th className="border text-sm">Role</th>
+                </tr>
+                <tr>
+                <td className="w-1/5 "><img src="./images/b0.png" width={40} className="m-auto"/></td>
+                  <td className="border text-xs"><p className="font-semibold">Number type card</p> <p>This card can be use to match previous drawed card either the card color or number</p></td>
+                </tr>
+                <tr>
+                <td className="w-1/5 "><img src="./images/gp2-0.png" width={40} className="m-auto"/></td>
+                  <td className="border text-xs"><p className="font-semibold">Plus two card</p>
+                    <p>When this card drawed, The next player will be added 2 card into their deck.</p>
+                  </td>
+                </tr>
+                <tr>
+                <td className="w-1/5 "><img src="./images/rS-0.png" width={40} className="m-auto"/></td>
+                  <td className="border text-xs"><p className="font-semibold">Skip card</p> 
+                  <p>When this card drawed, The turn of next player will be skipped</p></td>
+                </tr>
+                <tr>
+                <td className="w-1/5 "><img src="./images/yR.png" width={40} className="m-auto"/></td>
+                  <td className="border text-xs">Role</td>
+                </tr>
+                <tr>
+                <td className="w-1/5 "><img src="./images/wc.png" width={40} className="m-auto"/></td>
+                  <td className="border text-xs">Role</td>
+                </tr>
+              </table>
+            </div>
+          </div>
+      </Modal.Body>
+    </Modal>
       <div className="fixed top-0 right-1 p-2 m-2 rounded-lg flex justify-center">
+        <button className="py-2 px-3 bg-yellow-200 rounded-lg mr-4 font-semibold text-gray-700 hover:bg-yellow-300"
+         onClick={showModal}
+        >Help ?</button>
         {direction ? (
           <>
             {direction == "right" ? (
